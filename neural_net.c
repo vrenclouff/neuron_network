1e3a7801a6ede855813edaf737a1a66b22b39cc4
Lukas <luc.cerny@gmail.com>
2014-12-23 00:22:52 +0000

    Kontrola propojen

--- neural_net.c
+++ neural_net.c
@@ -14,8 +14,12 @@ void run(){
   printf("Loanding and crating neuron network...\n");
 
   neuron root, val;
+  edge val_edge;
+
+  val_edge.index = 1;
 
   val.index = 5;
+  val.edge = &val_edge;
   val.activation = count_activation();
 
   push_neuron(&root, &val);
--- neuron.c
+++ neuron.c
@@ -6,5 +6,6 @@ void push_neuron(neuron *root, neuron *val){
 
   printf("Zadany index je: %d\n", val->index);
   printf("Aktivace: %f\n", val->activation);
+  printf("Hrana: %d\n", val->edge->index);
 
 }
